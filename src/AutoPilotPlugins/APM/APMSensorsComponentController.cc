3b91411ca2d5cc357b3a543574606ea4b7ce51eb
SuWeipeng <weipeng.su@outlook.com>
2017-07-08 19:17:26 +0800

    禁用罗盘板上(Onboard)校准

--- src/AutoPilotPlugins/APM/APMSensorsComponentController.cc
+++ src/AutoPilotPlugins/APM/APMSensorsComponentController.cc
@@ -214,7 +214,7 @@ void APMSensorsComponentController::_mavCommandResult(int vehicleId, int compone
 
     if (command == MAV_CMD_DO_CANCEL_MAG_CAL) {
         disconnect(_vehicle, &Vehicle::mavCommandResult, this, &APMSensorsComponentController::_mavCommandResult);
-        if (result == MAV_RESULT_ACCEPTED) {
+        if (result == MAV_RESULT_ACCEPTED && 0) {
             // Onboard mag cal is supported
             _calTypeInProgress = CalTypeOnboardCompass;
             _rgCompassCalProgress[0] = 0;
