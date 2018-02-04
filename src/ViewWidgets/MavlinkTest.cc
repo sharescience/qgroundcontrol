1ca2e34ca20f9edf0c6f718452a31dd5856f1b7f
SuWeipeng <weipeng.su@outlook.com>
2018-02-04 17:50:15 +0800

    修正Ubuntu获取屏幕分辨率代码编译出错的问题

--- src/ViewWidgets/MavlinkTest.cc
+++ src/ViewWidgets/MavlinkTest.cc
@@ -15,9 +15,14 @@ MavlinkTest::MavlinkTest(const QString& title, QAction* action, QWidget *parent)
 {
     Q_UNUSED(title);
     Q_UNUSED(action);
-    int screenWidth=QApplication::desktop()->width();
-    int screenHeight=QApplication::desktop()->height();
+
+    // Adjust the size
+    QScreen* scr = QApplication::primaryScreen();
+    QSize scrSize = scr->availableSize();
+    int screenWidth=scrSize.width();
+    int screenHeight=scrSize.height();
     this->resize(screenWidth*8/15, screenHeight*4/5);
+
     setSource(QUrl::fromUserInput("qrc:/qml/MavlinkTest.qml"));
     loadSettings();
 }
